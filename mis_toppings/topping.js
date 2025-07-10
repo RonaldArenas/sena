823e89a8e73ab059138e35fb338f55d55d7a343c
Ronald <ronalyamel@hotmail.com>
2025-07-10 15:40:23 -0500

    Ejercicio Toppings

--- mis_toppings/topping.js
+++ mis_toppings/topping.js
@@ -1,2 +1,24 @@
-const titulo = document.getElementById('titulo');
-console.log(titulo.tagName);
+/* SELECCIONAR LOS ELEMENTOS DEL DOM */
+
+const lechuga = document.getElementById('lechuga');
+const cebolla = document.getElementById('cebolla');
+const albahaca = document.getElementById('albahaca');
+const tomate = document.getElementById('tomate');
+const mybody = document.getElementById('myBody');
+
+lechuga.addEventListener('click', () => {
+  mybody.style.backgroundImage = "url('imagenes/lechuga.jpeg')";
+});
+
+cebolla.addEventListener('click', () => {
+  mybody.style.backgroundImage = "url('imagenes/cebolla.jpg')";
+});
+
+albahaca.addEventListener('click', () => {
+  mybody.style.backgroundImage = "url('imagenes/albahaca.jpeg')";
+});
+
+tomate.addEventListener('click', () => {
+  mybody.style.backgroundImage = "url('imagenes/tomate.jpg')";
+});
+
